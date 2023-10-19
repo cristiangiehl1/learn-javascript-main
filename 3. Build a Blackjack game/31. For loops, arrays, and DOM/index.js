8f1222c3386af9fc315e459a0adfc850374f45c7
Cristian Giehl <cristiangiehl@gmail.com>
2023-10-19 16:19:04 -0300

    att

--- 3. Build a Blackjack game/31. For loops, arrays, and DOM/index.js
+++
@@ -1,7 +0,0 @@
-let sentence = ["Hello ", "my ", "name ", "is ", "Per"] 
-let greetingEl = document.getElementById("greeting-el")
-
-// Render the sentence in the greetingEl paragraph using a for loop and .textContent
-for (let index = 0; index < sentence.length; index += 1) {
-	greetingEl.textContent += sentence[index]
-}
