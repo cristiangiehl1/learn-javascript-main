6b885b3c1fa78948d60d9138d070e6ba4e9ec151
Cristian Giehl <cristiangiehl@gmail.com>
2023-10-19 16:20:19 -0300

    att

---
+++ 3. Build a Blackjack game/28. Write your first loop/index.js
@@ -0,0 +1,7 @@
+
+
+// Create a for loop that counts from 10 to 100 in steps of 10
+// Use console.log to log out the numbers
+for (let index = 10; index <= 100; index += 1) {
+	console.log(index);
+}
